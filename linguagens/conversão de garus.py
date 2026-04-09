a63984312456cf1925562518a6da7b3403ffb69f
ryancamaraviana <ryancamaraviana2016@gmail.com>
2026-04-09 01:51:09 -0300

    Delete linguagens/conversão de garus.py

--- linguagens/conversão de garus.py
+++
@@ -1,4 +0,0 @@
-print("Conversão de celsius para fahrenheit")
-celsius = float(input("Digite a temperatura em celsius: "))
-fahrenheit = (celsius * 9/5) + 32
-print(f"A temperatura em fahrenheit é: {fahrenheit}")
